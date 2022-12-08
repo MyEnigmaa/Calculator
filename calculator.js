169bb125e582ce36b408ea5575954c61cb45b7b3
Nik <n.koeck@gmx.de>
2022-12-08 15:49:12 +0100

    add style and eventHandler to button

--- calculator.js
+++ calculator.js
@@ -1,18 +1,27 @@
 const buttons = document.getElementById('buttons');
+const outputWindow = document.getElementById('output');
+let numberOne = 0;
+let numberTwo = 0;
+let solution = 0;
+let set = false;
 
 
-
-
-function Buttons (){
+function Buttons (){ //create 10 Buttons
     for(i=0; i<10; i++){
         number = document.createElement("button");
         number.value = i;
-        number.id = i;
         number.textContent = i;
         number.type = "button";
+        number.addEventListener("click", inputNumber);
         buttons.appendChild(number);
     }
 }
+
+function inputNumber(e){ //Setting values for calculation
+    (set) ? numberTwo = e.target.value: numberOne = e.target.value;
+}
+
+
 
 window.onload = () => {
     Buttons();
